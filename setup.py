d071e6d72d214858f1f9418b8c0ac55f7f582061
Adam Dodge <adam.dodge@colorado.edu>
2019-01-22 20:06:06 -0700

    updated for ARGUS

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from distutils.core import setup, Extension
 setup(
-    name='ARGUS',
+    name='cpredict',
     version='1.0',
     author="Adam Dodge",
     author_email="adam.dodge@colorado.edu",
     url="https://github.com/addodge/ARGUS",
-    py_modules=['predict'],
     ext_modules=[Extension('cpredict', ['predict.c'])]
     )

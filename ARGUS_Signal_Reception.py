85c6904011208d09974722c2be5420401ab6f325
Adam Dodge <adam.dodge@colorado.edu>
2019-03-03 11:52:32 -0700

    Fixed signal processing

--- ARGUS_Signal_Reception.py
+++ ARGUS_Signal_Reception.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: ARGUS_Signal_Reception
-# Generated: Tue Jan 29 13:23:56 2019
+# Generated: Sun Mar  3 11:51:50 2019
 ##################################################
 
 from distutils.version import StrictVersion
@@ -20,16 +20,15 @@ if __name__ == '__main__':
 
 from PyQt5 import Qt
 from PyQt5 import Qt, QtCore
-from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
+from gnuradio import iio
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
-import numpy
 import sip
 import sys
 from gnuradio import qtgui
@@ -68,8 +67,9 @@ class ARGUS_Signal_Reception(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 500
-        self.gui_samprate = gui_samprate = 4e6
+        self.samp_rate = samp_rate = 6e6
+        self.data_rate = data_rate = 2e6
+        self.carrier = carrier = 2.4e9
 
         ##################################################
         # Blocks
@@ -165,49 +165,30 @@ class ARGUS_Signal_Reception(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.digital_psk_mod_0 = digital.psk.psk_mod(
+        self.pluto_source_0 = iio.pluto_source('', int(carrier), int(samp_rate), int(data_rate), 0x8000, True, True, True, "manual", 64.0, '', True)
+        self.digital_psk_demod_1 = digital.psk.psk_demod(
           constellation_points=4,
-          mod_code="none",
-          differential=False,
-          samples_per_symbol=10,
-          excess_bw=0.35,
-          verbose=True,
-          log=False,
-          )
-        self.digital_psk_demod_0 = digital.psk.psk_demod(
-          constellation_points=4,
-          differential=False,
-          samples_per_symbol=10,
+          differential=True,
+          samples_per_symbol=3,
           excess_bw=0.35,
           phase_bw=6.28/100.0,
           timing_bw=6.28/100.0,
-          mod_code="none",
-          verbose=True,
+          mod_code="gray",
+          verbose=False,
           log=False,
           )
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, 'test2', False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, 'ARGUSdata.bin', False)
         self.blocks_file_sink_0_0.set_unbuffered(True)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'test1', False)
-        self.blocks_file_sink_0.set_unbuffered(True)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
-        self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 2, 10000000)), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.digital_psk_demod_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_1, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_char_to_float_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.digital_psk_mod_0, 0))
-        self.connect((self.digital_psk_demod_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.digital_psk_mod_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.digital_psk_demod_1, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.digital_psk_demod_1, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.pluto_source_0, 0), (self.digital_psk_demod_1, 0))
+        self.connect((self.pluto_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "ARGUS_Signal_Reception")
@@ -221,13 +202,21 @@ class ARGUS_Signal_Reception(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.pluto_source_0.set_params(int(self.carrier), int(self.samp_rate), int(self.data_rate), True, True, True, "manual", 64.0, '', True)
 
-    def get_gui_samprate(self):
-        return self.gui_samprate
+    def get_data_rate(self):
+        return self.data_rate
 
-    def set_gui_samprate(self, gui_samprate):
-        self.gui_samprate = gui_samprate
+    def set_data_rate(self, data_rate):
+        self.data_rate = data_rate
+        self.pluto_source_0.set_params(int(self.carrier), int(self.samp_rate), int(self.data_rate), True, True, True, "manual", 64.0, '', True)
+
+    def get_carrier(self):
+        return self.carrier
+
+    def set_carrier(self, carrier):
+        self.carrier = carrier
+        self.pluto_source_0.set_params(int(self.carrier), int(self.samp_rate), int(self.data_rate), True, True, True, "manual", 64.0, '', True)
 
 
 def main(top_block_cls=ARGUS_Signal_Reception, options=None):
